9d31c82ca4b6389c15f1c523d21e1b7f0a950bff
foultrip <davidvasquezmahecha@gmail.com>
2024-05-10 15:52:39 -0500

    pep8 correction

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""function add that takes a float a 
-and a float b as arguments and 
+"""function add that takes a float a
+and a float b as arguments and
 returns their sum as a float."""
 
 
