ce2f39281a8061198f4d0e44a29ae70b2f3749dd
foultrip <davidvasquezmahecha@gmail.com>
2024-05-15 19:25:52 -0500

    act01

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """Return an empty list if no document in the collection"""
+    print(mongo_collection)
+    return [x for x in mongo_collection.find()]
