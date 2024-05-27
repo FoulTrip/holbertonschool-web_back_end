da3725955f850d34e80233fee60dc1e067981da8
foultrip <davidvasquezmahecha@gmail.com>
2024-05-27 16:30:31 -0500

    act03

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set name(strings) {
+    if (typeof strings !== "string") {
+      throw new TypeError("Name must be a string");
+    }
+    this._name = strings;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("Length must be a number");
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError("Students must be an array");
+    }
+    this._students = students;
+  }
+}
