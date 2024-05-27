3aae825924aad158fabba1c1da88bd64d263dfbe
foultrip <davidvasquezmahecha@gmail.com>
2024-05-27 15:38:39 -0500

    function fixed

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from "./0-classroom";
 
-function initializeRooms() {
+export default function initializeRooms() {
   const classOne = new ClassRoom(19);
   const classTwo = new ClassRoom(20);
   const classThree = new ClassRoom(34);
