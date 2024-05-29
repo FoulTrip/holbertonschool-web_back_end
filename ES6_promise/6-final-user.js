9cf4a01aa61db788e10f0ac3f97a572bcb536b23
foultrip <davidvasquezmahecha@gmail.com>
2024-05-29 11:04:49 -0500

    6 fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,24 +1,30 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 async function handleProfileSignup(firstName, lastName, fileName) {
-  const promiseOne = {};
-  const promiseTwo = {};
+  const promiseOne = {
+    status: "",
+    value: "",
+  };
+  const promiseTwo = {
+    status: "",
+    value: "",
+  };
 
   try {
     promiseOne.value = await signUpUser(firstName, lastName);
-    promiseOne.status = 'fulfilled';
+    promiseOne.status = "fulfilled";
   } catch (err) {
     promiseOne.value = err.toString();
-    promiseOne.status = 'rejected';
+    promiseOne.status = "rejected";
   }
 
   try {
     promiseTwo.value = await uploadPhoto(fileName);
-    promiseTwo.status = 'fulfilled';
+    promiseTwo.status = "fulfilled";
   } catch (err) {
     promiseTwo.value = err.toString();
-    promiseTwo.status = 'rejected';
+    promiseTwo.status = "rejected";
   }
 
   return [promiseOne, promiseTwo];
